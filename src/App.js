5a4b83c0318a669ed955265b6627f99930928a02
Esteban <ematiasortiz@gmail.com>
2023-04-25 22:52:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const lyric = [
+	'Re...',
+	'Re...',
+	'Reina',
+	'Mi amor',
+	'Yo te extraño',
+	'bebe',
+	'Que',
+	'Hpta',
+	'Chimba',
+	'Seria',
+	'Vernos',
+	'Otra vez',
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentLyricText, setCurrentLyricText] = useState(0);
+	return (
+		<div className="App">
+			{currentLyricText < lyric.length ? (
+				<>
+					<Typography variant="h6" gutterBottom>
+						{lyric[currentLyricText]}
+					</Typography>
+					<Button onClick={() => setCurrentLyricText(currentLyricText + 1)}>
+						Siguiente
+					</Button>
+				</>
+			) : (
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/B8sO-Xu0g5M?start=120"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
+			)}
+		</div>
+	);
 }
 
 export default App;
